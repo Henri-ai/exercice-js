e51c470fb1bbf7e334a0f23eb12db9f9a4c3cc63
Henri <henrigoret@hotmail.com>
2022-05-10 15:49:10 +0200

    exercice js

---
+++ test.js
@@ -0,0 +1,171 @@
+
+//let nom = window.prompt("Entrez votre nom");
+
+//let prenom = window.prompt("Entrez votre prénom");
+
+//window.confirm("Etes-vous un homme ?");
+
+//window.alert("Bonjour Monsieur " + nom +" "+ prenom +  " Bienvenue sur notre site web !");
+
+//******************************************************************************************** */
+//let a = "100";
+//window.alert("Ceci est une chaine de caractères:"+a)
+
+
+//let b = 100;
+//b--;
+//console.log(b);
+
+
+//let c = 1.00;
+//c*=100;
+//console.log(c);
+
+//let d=true;
+//d=!true;
+//console.log(d);
+
+//**********************************************************************************************
+// ex1-parité***********************************************************************************
+
+
+//let n=prompt('Entrez un nombre');
+
+//if (n%2==0) {
+
+//alert("ce nombre est pair");
+
+//}
+
+//else{
+
+//alert("ce nombre est impair");
+
+//}
+
+//ex2-age********************************************************************************************
+
+//let année=prompt ("Entrez votre année de naissance");
+
+
+//if (année<=2004)
+//{
+//alert("vous etes majeur")
+//}
+
+//else
+//{
+
+//alert("vous etes mineur")
+
+//}
+
+
+ //exercice3-calculette*******************************************************************************
+//let nombre1=prompt ("Saisir un nombre");
+//let nombre2=prompt ("Saisir un deuxieme nombre");
+//let signe=prompt ("Saisir un signe");
+
+//switch (signe)
+//{
+//case"+":
+//alert(parseInt(nombre1)+parseInt(nombre2));
+//break;
+
+//case"-":
+//alert(parseInt(nombre1)-parseInt(nombre2));
+//break;
+
+//case"*":
+//alert(parseInt(nombre1)*parseInt(nombre2));
+//break;
+
+//case"/":
+//alert(parseInt(nombre1)/parseInt(nombre2));
+//break;
+
+//default:
+//alert("Signe incorrect veuillez entrer les signes : +,-,* ou /");
+
+//}
+
+//********************************************************************************************************
+//Exercice-saisie
+
+//let numero=1;
+//let prenom
+
+//while (numero<=4)
+//{
+//let prenom= prompt("Saisissez le prenom n°"+numero+"ou clic sur annuler pour arreter la saisie");
+//console.log(prenom+numero);
+//numero++;
+//if (prenom<=4)
+//{
+//break;
+//}
+
+//}
+
+//console.log("fin")
+
+
+
+//exercice2***********************************************************************************************
+
+//let n=50
+//for (let n=50;n>=0;n--){
+
+//console.log(n);
+//}
+
+//exercice3************************************************************************************************
+
+
+            let nombre=0;
+			let somme=0;
+			let moyenne=0;
+			let nb_notes=0;
+		
+            for(nombre=0;nombre==nb_notes;nombre+nb_notes)
+            {
+            nb_notes=prompt("Souhaitez-vous faire la moyenne de combien de notes ?",nb_notes); 
+            nombre=prompt("entrez la note")
+                
+			moyenne=(somme/nb_notes);
+            alert("la moyenne des notes est :"+moyenne)
+            }
+console.log(nombre)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
